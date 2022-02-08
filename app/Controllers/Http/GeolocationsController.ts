08bd62ed5e81f46440edb09e699b5c68a631bfbd
king kobby <ampahkwabena5@gmail.com>
2022-02-08 16:32:23 +0000

    Created a route : /search-address

--- app/Controllers/Http/GeolocationsController.ts
+++ app/Controllers/Http/GeolocationsController.ts
@@ -1,3 +1,24 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from "@ioc:Adonis/Core/Validator";
+import Env from "@ioc:Adonis/Core/Env";
 
-export default class GeolocationsController {}
+export default class GeolocationsController {
+  public async index({ request, response }: HttpContextContract) {
+    /**
+     * Schema definition
+     */
+    const requestSchema = schema.create({
+      address: schema.string({ trim: true }),
+      email: schema.string({}, [rules.email()]),
+    });
+
+    /**
+     * Validate request body against the schema
+     */
+    const payload = await request.validate({ schema: requestSchema });
+
+    return payload;
+
+  }
+
+}
