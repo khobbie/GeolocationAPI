3dc1b2f66744c021ce0c23e4ef5f88a6d51cb2e1
king kobby <ampahkwabena5@gmail.com>
2022-02-08 16:35:30 +0000

    Added axio http to project

--- app/Controllers/Http/GeolocationsController.ts
+++ app/Controllers/Http/GeolocationsController.ts
@@ -1,8 +1,13 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema, rules } from "@ioc:Adonis/Core/Validator";
 import Env from "@ioc:Adonis/Core/Env";
+import axios from "axios";
+import Mail from "@ioc:Adonis/Addons/Mail";
+
 
 export default class GeolocationsController {
+
+
   public async index({ request, response }: HttpContextContract) {
     /**
      * Schema definition
@@ -17,8 +22,61 @@ export default class GeolocationsController {
      */
     const payload = await request.validate({ schema: requestSchema });
 
-    return payload;
+    let address = payload.address;
+    ("200 W 53rd St, New York, NY 10019, United States");
+    let email = payload.email;
 
+    let base_url = Env.get("LOCATION_BASE_URL");
+
+    try {
+      const res = await axios.get(base_url, {
+        params: {
+          text: address,
+          apiKey: Env.get("LOCATION_API_KEY"),
+        },
+      });
+      if (res.status == 200) {
+        console.log(res.data.features[0].properties.lon);
+        console.log(res.data.features[0].properties.lat);
+
+        let latitude = res.data.features[0].properties.lon;
+        let longitude = res.data.features[0].properties.lat;
+
+        await Mail.send((message) => {
+          message
+            .from("info@cruitin.com")
+            .to(email)
+            .subject("Geolocation Receives!")
+            // .text("HELLO")
+            .htmlView("emails/welcome", {
+              address: address,
+              latitude: latitude,
+              longitude: longitude,
+            });
+        });
+
+        return response.ok({
+          message: "Geolocation found",
+          data: {
+            latitude: latitude,
+            longitude: longitude,
+          },
+        });
+      } else {
+        return response.ok({
+          message: "Geolocation not found",
+          data: null,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+
+      return response.ok({
+        message: error.getMessage(),
+        data: null,
+      });
+    }
   }
+
 
 }
